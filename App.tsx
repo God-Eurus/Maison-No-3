40325bec919c4da0ff5e9cbb0e099b75a4e4d820
God-Eurus <maharwalgaurang@gmail.com>
2026-02-27 00:29:03 +0530

    getintouch fixes

--- App.tsx
+++ App.tsx
@@ -28,7 +28,7 @@ import ThirdHall from './src/pages/thirdhall';
 import ThirdHallForm from './src/pages/thirdhallform';
 import HallRecord from './src/pages/HallRecord';
 import Aia from './src/pages/aia';
-import GetInTouch from './src/pages/getintouch';
+
 import KezaVeraForm from './src/pages/KezaveraForm';
 
 function App() {
@@ -73,7 +73,7 @@ function App() {
           <Route path="/thirdhallform" element={<ThirdHallForm />} />
           <Route path="/hallrecord" element={<HallRecord />} />
           <Route path="/aia" element={<Aia />} />
-          <Route path="/getintouch" element={<GetInTouch />} />
+          
           <Route path="/kezaveraform" element={<KezaVeraForm />} />
 
 
